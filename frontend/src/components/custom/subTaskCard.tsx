50eb6479b5d5ce4294c3b0544a8fb85477cce5fd
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-10-08 01:44:26 +0530

    feat : conditional classes

--- frontend/src/components/custom/subTaskCard.tsx
+++ frontend/src/components/custom/subTaskCard.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import {useAuth} from "@/providers/user-auth-store-provider"
 import type {subTask} from "@/types"
 
 const SubTaskCard = ({subtask}:{subtask:subTask}) => {
 
+  const{isAdmin} = useAuth(state=>state);
+
   return (
-    <li className='p-1 bg-amber-300 rounded'>
-        <p className='text-sm text-gray-600'>
+    <li className={`
+     ${(isAdmin && subtask.id % 3 === 0) && "bg-emerald-300"} 
+      ${(isAdmin && subtask.id % 2 === 0) && "bg-red-300"} 
+      ${(isAdmin && subtask.id % 1 === 0) && "bg-blue-300"} 
+    group relative flex items-center justify-between p-1 rounded hover:bg-gray-100/50 transition-colors duration-200`}>
+        <p className='text-sm text-gray-600 truncate pr-2'>
            • {subtask.content} 
-        </p> 
+        </p>
+        <div className="absolute left-11/12 -bottom-5 -translate-y-1/2 z-10
+            flex items-center justify-center
+            h-6 bg-gray-200 text-gray-700 text-xs font-bold
+            rounded-full shadow-md transition-all duration-300 ease-in-out
+            w-6 group-hover:w-auto group-hover:px-3
+            overflow-hidden whitespace-nowrap cursor-default select-none
+          "
+        >
+          {subtask.content}
+        </div>
     </li>
   )
 }

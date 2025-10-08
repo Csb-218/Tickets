ff2893ef6d83560ed3c8047a1fc7bdf24fd413a5
CS BHAGWANT <csbhagwant@gmail.com>
2025-10-08 16:10:04 +0530

    feat(component) :  context provider for sidebar store

---
+++ frontend/src/providers/sidebar-store-provider.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { type ReactNode, createContext, useRef, useContext } from 'react'
+import { type StoreApi, useStore } from 'zustand'
+
+import { type SidebarStore, createSidebarStore } from '@/store/sidebarStore'
+
+export const SidebarStoreContext = createContext<StoreApi<SidebarStore> | null>(
+  null,
+)
+
+export interface SidebarStoreProviderProps {
+  children: ReactNode
+}
+
+export const SidebarStoreProvider = ({ children }: SidebarStoreProviderProps) => {
+  const storeRef = useRef<StoreApi<SidebarStore>|null>(null)
+  if (!storeRef.current) {
+    storeRef.current = createSidebarStore()
+  }
+
+  return (
+    <SidebarStoreContext.Provider value={storeRef.current}>
+      {children}
+    </SidebarStoreContext.Provider>
+  )
+}
+
+export const useSidebar = <T,>(selector: (store: SidebarStore) => T): T => {
+  const sidebarStoreContext = useContext(SidebarStoreContext)
+
+  if (!sidebarStoreContext) {
+    throw new Error(`useSidebar must be used within a SidebarStoreProvider`)
+  }
+
+  return useStore(sidebarStoreContext, selector)
+}

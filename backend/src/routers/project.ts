1fad6bae9d2022e8d0acbb562b93c501b98af4d0
CS BHAGWANT <csbhagwant@gmail.com>
2025-10-11 19:19:13 +0530

    perf : replaced controller on route

--- backend/src/routers/project.ts
+++ backend/src/routers/project.ts
@@ -1,11 +1,11 @@
 import {Router} from "express"
-import {createProject,getAllProjects,getProjectById,updateProject,deleteProject} from "../controllers/projectController"
+import {createProject,getAllProjects,getProjectById,updateProject,deleteProject,getProjectWithBoard} from "../controllers/projectController"
 
 const router = Router()
 
 router.post("/",createProject)
 router.get("/",getAllProjects)
-router.get("/:id",getProjectById)
+router.get("/:id",getProjectWithBoard)
 router.put("/:id",updateProject)
 router.delete("/:id",deleteProject)
 

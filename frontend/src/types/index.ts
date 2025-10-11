a612198b3747cae303d0b9eb5c3bb18bf0a58316
CS BHAGWANT <csbhagwant@gmail.com>
2025-10-11 19:23:01 +0530

    feat : introduced project types

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,16 +1,74 @@
 import type { VariantProps } from 'class-variance-authority';
 import { Badge } from "@/components/ui/badge"
 
-export interface subTask {
-    id:number
-    content: string
-    done: boolean
+export interface Project{
+    id: string;
+    name: string;
+    description: string | null;
+    createdAt: string;
+    updatedAt: string;
+    ownerId: string;
+    owner: User;
 }
 
+export type ProjectDetails = Project & {
+    members: ProjectMember[];
+    lists: (TaskList & { tasks: Task[] })[];
+};
 
-export interface Task{
-    id:number
-    title: string
-    subtasks: subTask[]
-        
+export type Role = "ADMIN" | "MEMBER";
+
+export interface ProjectMember {
+    id: string;
+    userId: string;
+    projectId: string;
+    role: Role;
+    joinedAt: string;
+    user: User;
+}
+
+export interface User {
+    id: string;
+    name: string | null;
+    avatarUrl: string | null;
+}
+
+export interface TaskList {
+    id: string;
+    name: string;
+    order: number;
+    projectId: string;
+}
+
+export type TaskStatus = "Todo" | "In Progress" | "Done";
+export type TaskPriority = "HIGH" | "MEDIUM" | "LOW";
+export type TaskWithSubtasks = Task & { subtasks: Subtask[] };
+
+export interface Task {
+    id: string;
+    title: string;
+    description: string | null;
+    order: number;
+    status: TaskStatus;
+    priority: TaskPriority;
+    dueDate: string | null;
+    version: number;
+    createdAt: string;
+    updatedAt: string;
+    listId: string;
+    projectId: string;
+    createdById: string;
+    assignedToId: string;
+    subtasks: Subtask[];
+}
+
+export interface Subtask {
+    id: string;
+    title: string;
+    isDone: boolean;
+    order: number;
+    createdAt: string;
+    updatedAt: string;
+    taskId: string;
+    assigneeId: string | null;
 }

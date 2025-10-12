ae0405e4bf93b090be56d93017fa8927a4f06461
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-10-13 00:27:53 +0530

    fix : halted providers

--- frontend/src/providers/sidebar-store-provider.tsx
+++ frontend/src/providers/sidebar-store-provider.tsx
@@ -1,37 +1,37 @@
-"use client"
+// "use client"
 
-import { type ReactNode, createContext, useRef, useContext } from 'react'
-import { type StoreApi, useStore } from 'zustand'
+// import { type ReactNode, createContext, useRef, useContext } from 'react'
+// import { type StoreApi, useStore } from 'zustand'
 
-import { type SidebarStore, createSidebarStore } from '@/store/sidebarStore'
+// import { type SidebarStore, createSidebarStor } from '@/store/sidebarStore'
 
-export const SidebarStoreContext = createContext<StoreApi<SidebarStore> | null>(
-  null,
-)
+// export const SidebarStoreContext = createContext<StoreApi<SidebarStore> | null>(
+//   null,
+// )
 
-export interface SidebarStoreProviderProps {
-  children: ReactNode
-}
+// export interface SidebarStoreProviderProps {
+//   children: ReactNode
+// }
 
-export const SidebarStoreProvider = ({ children }: SidebarStoreProviderProps) => {
-  const storeRef = useRef<StoreApi<SidebarStore>|null>(null)
-  if (!storeRef.current) {
-    storeRef.current = createSidebarStore()
-  }
+// export const SidebarStoreProvider = ({ children }: SidebarStoreProviderProps) => {
+//   const storeRef = useRef<StoreApi<SidebarStore>|null>(null)
+//   if (!storeRef.current) {
+//     storeRef.current = createSidebarStore()
+//   }
 
-  return (
-    <SidebarStoreContext.Provider value={storeRef.current}>
-      {children}
-    </SidebarStoreContext.Provider>
-  )
-}
+//   return (
+//     <SidebarStoreContext.Provider value={storeRef.current}>
+//       {children}
+//     </SidebarStoreContext.Provider>
+//   )
+// }
 
-export const useSidebar = <T,>(selector: (store: SidebarStore) => T): T => {
-  const sidebarStoreContext = useContext(SidebarStoreContext)
+// export const useSidebar = <T,>(selector: (store: SidebarStore) => T): T => {
+//   const sidebarStoreContext = useContext(SidebarStoreContext)
 
-  if (!sidebarStoreContext) {
-    throw new Error(`useSidebar must be used within a SidebarStoreProvider`)
-  }
+//   if (!sidebarStoreContext) {
+//     throw new Error(`useSidebar must be used within a SidebarStoreProvider`)
+//   }
 
-  return useStore(sidebarStoreContext, selector)
-}
+//   return useStore(sidebarStoreContext, selector)
+// }
--- frontend/src/providers/user-auth-store-provider.tsx
+++ frontend/src/providers/user-auth-store-provider.tsx
@@ -1,44 +1,44 @@
-// src/providers/counter-store-provider.tsx
-'use client'
+// // src/providers/counter-store-provider.tsx
+// 'use client'
 
-import { type ReactNode, createContext, useRef, useContext } from 'react'
-import { useStore } from 'zustand'
+// import { type ReactNode, createContext, useRef, useContext } from 'react'
+// import { useStore } from 'zustand'
 
-import { type UserAuthStore, createAuthStore } from '@/store/user-auth'
+// import { type UserAuthStore, createAuthSlice } from '@/store/user-auth'
 
-export type AuthStoreApi = ReturnType<typeof createAuthStore>
+// export type AuthStoreApi = ReturnType<typeof createAuthSlice>
 
-export const AuthStoreContext = createContext<AuthStoreApi | undefined>(
-  undefined,
-)
+// export const AuthStoreContext = createContext<AuthStoreApi | undefined>(
+//   undefined,
+// )
 
-export interface AuthStoreProviderProps {
-  children: ReactNode
-}
+// export interface AuthStoreProviderProps {
+//   children: ReactNode
+// }
 
-export const AuthStoreProvider = ({
-  children,
-}: AuthStoreProviderProps) => {
-  const storeRef = useRef<AuthStoreApi | null>(null)
-  if (storeRef.current === null) {
-    storeRef.current = createAuthStore()
-  }
+// export const AuthStoreProvider = ({
+//   children,
+// }: AuthStoreProviderProps) => {
+//   const storeRef = useRef<AuthStoreApi | null>(null)
+//   if (storeRef.current === null) {
+//     storeRef.current = createAuthSlice()
+//   }
 
-  return (
-    <AuthStoreContext.Provider value={storeRef.current}>
-      {children}
-    </AuthStoreContext.Provider>
-  )
-}
+//   return (
+//     <AuthStoreContext.Provider value={storeRef.current}>
+//       {children}
+//     </AuthStoreContext.Provider>
+//   )
+// }
 
-export const useAuth = <T,>(
-  selector: (store: UserAuthStore) => T,
-): T => {
-  const authStoreContext = useContext(AuthStoreContext)
+// export const useAuth = <T,>(
+//   selector: (store: UserAuthStore) => T,
+// ): T => {
+//   const authStoreContext = useContext(AuthStoreContext)
 
-  if (!authStoreContext) {
-    throw new Error(`useCounterStore must be used within CounterStoreProvider`)
-  }
+//   if (!authStoreContext) {
+//     throw new Error(`useCounterStore must be used within CounterStoreProvider`)
+//   }
 
-  return useStore(authStoreContext, selector)
-}
+//   return useStore(authStoreContext, selector)
+// }

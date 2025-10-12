1243326c850067134402822b41bef637bb55bd0d
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-10-12 19:29:26 +0530

    Create loading.tsx

---
+++ frontend/src/app/dashboard/team/[project]/loading.tsx
@@ -0,0 +1,8 @@
+import { Spinner } from "@/components/ui/spinner"
+
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return <div className="h-full w-full flex flex-col justify-center items-center"><Spinner/></div>
+
+  
+}
